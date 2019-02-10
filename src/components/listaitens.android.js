840fd72ef886ca30ea61fd51b35acb5bc4126e72
Patric Pereira <patric.pereirafranca@gmail.com>
2019-02-09 23:05:43 -0200

    efetuando requisições http com o axios

--- src/components/listaitens.android.js
+++ src/components/listaitens.android.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 
+import axios from 'axios';
+
 import Itens from './itens.android';
 
 export default class ListaItens extends Component {
+
+  componentWillMount() {
+    axios.get('http://faus.com.br/recursos/c/dmairr/api/itens.html')
+      .then((response) => console.log(response))
+      .catch(() => console.log('Erro ao recuperar os dados'));
+  }
 
   render() {
     return (
