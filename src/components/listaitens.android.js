be7e8ee842c038b7d2908a4f1f8ed266897bff1c
Patric Pereira <patric.pereirafranca@gmail.com>
2019-02-09 22:19:15 -0200

    ciclo de vida dos componentes

--- src/components/listaitens.android.js
+++ src/components/listaitens.android.js
@@ -2,7 +2,22 @@ import React, { Component } from 'react';
 import { Text } from 'react-native';
 
 export default class ListaItens extends Component {
+
+  constructor(props) {
+    super(props);
+    console.log('Construindo...');
+  }
+
+  componentWillMount() {
+    console.log('Fazer algo antes de renderizar');
+  }
+
+  componentDidMount() {
+    console.log('depois de renderizar');
+  }
+
   render() {
+    console.log('renderizado');
     return (
       <Text>Importação de componente</Text>
     );
